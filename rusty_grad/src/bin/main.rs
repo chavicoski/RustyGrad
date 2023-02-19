1ba51c6c2b987a8e629dd33776e2c5230b5a9c50
chavicoski <alvaro.lopez.chilet@gmail.com>
2023-02-19 21:49:12 +0100

    Added basic operations (just forward)

--- rusty_grad/src/bin/main.rs
+++ rusty_grad/src/bin/main.rs
@@ -1,6 +1,21 @@
-use rusty_grad;
+use rusty_grad::Value;
 
 fn main() {
-    let a = rusty_grad::add(1, 2);
-    println!("a={a}");
+    // Inputs
+    let x1 = Value::new(2.0);
+    let x2 = Value::new(0.0);
+
+    // Weights
+    let w1 = Value::new(-3.0);
+    let w2 = Value::new(1.0);
+    // Bias
+    let b = Value::new(6.8813735870195432);
+
+    // Compute: x1*w1 + x2*w2 + b
+    let x1w1 = &x1 * &w1;
+    let x2w2 = &x2 * &w2;
+    let x1w1_x2w2 = &x1w1 + &x2w2;
+    let n = &x1w1_x2w2 + &b;
+    let out = n.tanh();
+    println!("output = {out}");
 }
--- rusty_grad/src/lib.rs
+++ rusty_grad/src/lib.rs
@@ -1,5 +1,75 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use core::fmt;
+use std::ops;
+
+pub struct Value<'a> {
+    pub data: f32,
+    grad: f32,
+    prev: Option<Vec<&'a Value<'a>>>,
+    op: Option<Op>,
+}
+
+enum Op {
+    Add,
+    Mul,
+    Tanh,
+}
+
+impl Value<'_> {
+    pub fn new(value: f32) -> Self {
+        Value {
+            data: value,
+            grad: 0.0,
+            prev: None,
+            op: None,
+        }
+    }
+
+    pub fn set_grad(&mut self, grad: f32) {
+        self.grad = grad;
+    }
+
+    pub fn tanh<'a>(&'a self) -> Value<'a> {
+        let aux_exp = f32::exp(2.0 * self.data);
+        let t = (aux_exp - 1.0) / (aux_exp + 1.0);
+        Value {
+            data: t,
+            grad: 0.0,
+            prev: Some(vec![self]),
+            op: Some(Op::Tanh),
+        }
+    }
+}
+
+impl fmt::Display for Value<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Value(data={})", self.data)
+    }
+}
+
+impl<'a> ops::Add<&'a Value<'a>> for &'a Value<'a> {
+    type Output = Value<'a>;
+
+    fn add(self, rhs: &'a Value) -> Self::Output {
+        Value {
+            data: self.data + rhs.data,
+            grad: 0.0,
+            prev: Some(vec![&self, &rhs]),
+            op: Some(Op::Add),
+        }
+    }
+}
+
+impl<'a> ops::Mul<&'a Value<'a>> for &'a Value<'a> {
+    type Output = Value<'a>;
+
+    fn mul(self, rhs: &'a Value) -> Self::Output {
+        Value {
+            data: self.data * rhs.data,
+            grad: 0.0,
+            prev: Some(vec![&self, &rhs]),
+            op: Some(Op::Mul),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +77,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn values_add_ok() {
+        let v1 = Value::new(27.0);
+        let v2 = Value::new(23.0);
+        let v3 = &v1 + &v2;
+        assert_eq!(v3.data, 50.0);
+    }
+
+    #[test]
+    fn values_mul_ok() {
+        let v1 = Value::new(2.0);
+        let v2 = Value::new(6.0);
+        let v3 = &v1 * &v2;
+        assert_eq!(v3.data, 12.0);
+    }
+
+    #[test]
+    fn value_tanh_ok() {
+        let v = Value::new(0.0);
+        let tanh = v.tanh();
+        assert_eq!(tanh.data, 0.0);
+
+        let v = Value::new(0.7);
+        let tanh = v.tanh();
+        assert_eq!(tanh.data, 0.6043678);
     }
 }
