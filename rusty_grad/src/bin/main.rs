7d15abc04ab8f5092b355275c07eac8d176ae499
chavicoski <alvaro.lopez.chilet@gmail.com>
2023-02-21 19:46:26 +0100

    Changed Value estruct to use Rc<RefCell<Value>>

--- rusty_grad/src/bin/main.rs
+++ rusty_grad/src/bin/main.rs
@@ -1,23 +1,25 @@
 use std::cell::RefCell;
+use std::rc::Rc;
 
 use rusty_grad::{add, mul, tanh, Value};
 
 fn main() {
     // Inputs
-    let x1 = RefCell::new(Value::new(2.0));
-    let x2 = RefCell::new(Value::new(0.0));
+    let x1 = Rc::new(RefCell::new(Value::new(2.0)));
+    let x2 = Rc::new(RefCell::new(Value::new(0.0)));
 
     // Weights
-    let w1 = RefCell::new(Value::new(-3.0));
-    let w2 = RefCell::new(Value::new(1.0));
+    let w1 = Rc::new(RefCell::new(Value::new(-3.0)));
+    let w2 = Rc::new(RefCell::new(Value::new(1.0)));
     // Bias
-    let b = RefCell::new(Value::new(6.8813735870195432));
+    let b = Rc::new(RefCell::new(Value::new(6.8813735870195432)));
 
     // Compute: x1*w1 + x2*w2 + b
-    let x1w1 = mul(&x1, &w1);
-    let x2w2 = mul(&x2, &w2);
-    let x1w1_x2w2 = add(&x1w1, &x2w2);
-    let n = add(&x1w1_x2w2, &b);
-    let out = tanh(&n).into_inner();
+    let x1w1 = mul(x1, w1);
+    let x2w2 = mul(x2, w2);
+    let x1w1_x2w2 = add(x1w1, x2w2);
+    let n = add(x1w1_x2w2, b);
+    let out = tanh(n);
+    let out = out.borrow();
     println!("output = {out}");
 }
--- rusty_grad/src/lib.rs
+++ rusty_grad/src/lib.rs
@@ -1,10 +1,11 @@
 use core::fmt;
 use std::cell::RefCell;
+use std::rc::Rc;
 
-pub struct Value<'a> {
+pub struct Value {
     pub data: f32,
     pub grad: f32,
-    prev: Option<Vec<&'a RefCell<Value<'a>>>>,
+    prev: Option<Vec<Rc<RefCell<Value>>>>,
     op: Option<Op>,
 }
 
@@ -14,7 +15,7 @@ enum Op {
     Tanh,
 }
 
-impl Value<'_> {
+impl Value {
     pub fn new(value: f32) -> Self {
         Value {
             data: value,
@@ -25,22 +26,22 @@ impl Value<'_> {
     }
 }
 
-impl fmt::Display for Value<'_> {
+impl fmt::Display for Value {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "Value(data={}, grad={})", self.data, self.grad)
     }
 }
 
-pub fn add<'a>(v1: &'a RefCell<Value<'a>>, v2: &'a RefCell<Value<'a>>) -> RefCell<Value<'a>> {
-    RefCell::new(Value {
+pub fn add(v1: Rc<RefCell<Value>>, v2: Rc<RefCell<Value>>) -> Rc<RefCell<Value>> {
+    Rc::new(RefCell::new(Value {
         data: v1.borrow().data + v2.borrow().data,
         grad: 0.0,
-        prev: Some(vec![v1, v2]),
+        prev: Some(vec![v1.clone(), v2.clone()]),
         op: Some(Op::Add),
-    })
+    }))
 }
 
-fn add_backward(value: &RefCell<Value>) {
+fn add_backward(value: Rc<RefCell<Value>>) {
     let value = value.borrow();
     match &value.prev {
         Some(children) => {
@@ -52,20 +53,20 @@ fn add_backward(value: &RefCell<Value>) {
     }
 }
 
-pub fn mul<'a>(v1: &'a RefCell<Value<'a>>, v2: &'a RefCell<Value<'a>>) -> RefCell<Value<'a>> {
-    RefCell::new(Value {
+pub fn mul(v1: Rc<RefCell<Value>>, v2: Rc<RefCell<Value>>) -> Rc<RefCell<Value>> {
+    Rc::new(RefCell::new(Value {
         data: v1.borrow().data * v2.borrow().data,
         grad: 0.0,
-        prev: Some(vec![v1, v2]),
+        prev: Some(vec![v1.clone(), v2.clone()]),
         op: Some(Op::Mul),
-    })
+    }))
 }
 
-fn mul_backward(value: &RefCell<Value>) {
+fn mul_backward(value: Rc<RefCell<Value>>) {
     let value = value.borrow();
     match &value.prev {
         Some(children) => match &children[..] {
-            &[v1, v2] => {
+            [v1, v2] => {
                 let mut v1 = v1.borrow_mut();
                 let mut v2 = v2.borrow_mut();
                 v1.grad += value.grad * v2.data;
@@ -77,22 +78,22 @@ fn mul_backward(value: &RefCell<Value>) {
     }
 }
 
-pub fn tanh<'a>(value: &'a RefCell<Value<'a>>) -> RefCell<Value<'a>> {
+pub fn tanh(value: Rc<RefCell<Value>>) -> Rc<RefCell<Value>> {
     let aux_exp = f32::exp(2.0 * value.borrow().data);
     let t = (aux_exp - 1.0) / (aux_exp + 1.0);
-    RefCell::new(Value {
+    Rc::new(RefCell::new(Value {
         data: t,
         grad: 0.0,
         prev: Some(vec![value]),
         op: Some(Op::Tanh),
-    })
+    }))
 }
 
-fn tanh_backward(value: &RefCell<Value>) {
+fn tanh_backward(value: Rc<RefCell<Value>>) {
     let value = value.borrow();
     match &value.prev {
         Some(children) => match &children[..] {
-            &[v] => {
+            [v] => {
                 let mut v = v.borrow_mut();
                 v.grad += value.grad * (1.0 - f32::powi(value.data, 2));
             }
@@ -108,65 +109,65 @@ mod tests {
 
     #[test]
     fn add_ok() {
-        let v1 = RefCell::new(Value::new(27.0));
-        let v2 = RefCell::new(Value::new(23.0));
-        let v3 = add(&v1, &v2);
+        let v1 = Rc::new(RefCell::new(Value::new(27.0)));
+        let v2 = Rc::new(RefCell::new(Value::new(23.0)));
+        let v3 = add(v1, v2);
         assert_eq!(v3.borrow().data, 50.0);
     }
 
     #[test]
     fn add_backward_ok() {
-        let v1 = RefCell::new(Value::new(21.0));
-        let v2 = RefCell::new(Value::new(12.0));
-        let v3 = add(&v1, &v2);
+        let v1 = Rc::new(RefCell::new(Value::new(21.0)));
+        let v2 = Rc::new(RefCell::new(Value::new(12.0)));
+        let v3 = add(v1.clone(), v2.clone());
         assert_eq!(v3.borrow().data, 33.0);
 
         v3.borrow_mut().grad = 3.0;
-        add_backward(&v3);
+        add_backward(v3);
         assert_eq!(v1.borrow().grad, 3.0);
         assert_eq!(v2.borrow().grad, 3.0);
     }
 
     #[test]
     fn mul_ok() {
-        let v1 = RefCell::new(Value::new(2.0));
-        let v2 = RefCell::new(Value::new(6.0));
-        let v3 = mul(&v1, &v2);
+        let v1 = Rc::new(RefCell::new(Value::new(2.0)));
+        let v2 = Rc::new(RefCell::new(Value::new(6.0)));
+        let v3 = mul(v1, v2);
         assert_eq!(v3.borrow().data, 12.0);
     }
 
     #[test]
     fn mul_backward_ok() {
-        let v1 = RefCell::new(Value::new(2.0));
-        let v2 = RefCell::new(Value::new(6.0));
-        let v3 = mul(&v1, &v2);
+        let v1 = Rc::new(RefCell::new(Value::new(2.0)));
+        let v2 = Rc::new(RefCell::new(Value::new(6.0)));
+        let v3 = mul(v1.clone(), v2.clone());
         assert_eq!(v3.borrow().data, 12.0);
 
         v3.borrow_mut().grad = 2.0;
-        mul_backward(&v3);
+        mul_backward(v3);
         assert_eq!(v1.borrow().grad, 12.0);
         assert_eq!(v2.borrow().grad, 4.0);
     }
 
     #[test]
     fn tanh_ok() {
-        let v1 = RefCell::new(Value::new(0.0));
-        let v2 = tanh(&v1);
+        let v1 = Rc::new(RefCell::new(Value::new(0.0)));
+        let v2 = tanh(v1);
         assert_eq!(v2.borrow().data, 0.0);
 
-        let v1 = RefCell::new(Value::new(0.7));
-        let v2 = tanh(&v1);
+        let v1 = Rc::new(RefCell::new(Value::new(0.7)));
+        let v2 = tanh(v1);
         assert_eq!(v2.borrow().data, 0.6043678);
     }
 
     #[test]
     fn tanh_backward_ok() {
-        let input = RefCell::new(Value::new(0.8814));
-        let out = tanh(&input);
+        let input = Rc::new(RefCell::new(Value::new(0.8814)));
+        let out = tanh(input.clone());
         assert_eq!(out.borrow().data, 0.70712);
 
         out.borrow_mut().grad = 1.0;
-        tanh_backward(&out);
+        tanh_backward(out);
         assert_eq!(input.borrow().grad, 0.49998128);
     }
 }
